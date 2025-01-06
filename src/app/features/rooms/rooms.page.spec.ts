855acf90de7ecf5460dcb52e81adbac598cfa4d0
Amanda Gabriela <amandagabriela056@gmail.com>
2025-01-05 21:39:16 -0300

    add os components de pagina inicial e comodos, juntamente com configurações das rotas

--- src/app/features/rooms/rooms.page.spec.ts
+++ src/app/features/rooms/rooms.page.spec.ts
@@ -1,13 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RoomsPage } from './rooms.page';
 
-import { Tab2Page } from './tab2.page';
+describe('RoomsPage', () => {
+  let component: RoomsPage;
+  let fixture: ComponentFixture<RoomsPage>;
 
-describe('Tab2Page', () => {
-  let component: Tab2Page;
-  let fixture: ComponentFixture<Tab2Page>;
-
-  beforeEach(async () => {
-    fixture = TestBed.createComponent(Tab2Page);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RoomsPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -7,30 +7,28 @@ export const routes: Routes = [
     component: TabsPage,
     children: [
       {
+        path: 'home',
+        loadComponent: () => import('./../features/home/home.page').then( m => m.HomePage)
+      },
+      {
+        path: 'rooms',
+        loadComponent: () => import('./../features/rooms/rooms.page').then( m => m.RoomsPage)
+      },
+      {
         path: 'tab1',
         loadComponent: () =>
           import('../tab1/tab1.page').then((m) => m.Tab1Page),
       },
       {
-        path: 'tab2',
-        loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
-      },
-      {
-        path: 'tab3',
-        loadComponent: () =>
-          import('../tab3/tab3.page').then((m) => m.Tab3Page),
-      },
-      {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/home',
         pathMatch: 'full',
       },
     ],
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/home',
     pathMatch: 'full',
   },
 ];
